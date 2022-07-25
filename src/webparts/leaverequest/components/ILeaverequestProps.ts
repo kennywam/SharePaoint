7463382a9378fb9d4b3c17b5d19b31986e82334b
Kennedy <kennywambu254@gmail.com>
2022-07-25 15:34:26 +0300

    pushing v1.0

---
+++ src/webparts/leaverequest/components/ILeaverequestProps.ts
@@ -0,0 +1,7 @@
+export interface ILeaverequestProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/leaverequest/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ILeaverequestWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'LeaverequestWebPartStrings' {
+  const strings: ILeaverequestWebPartStrings;
+  export = strings;
+}
